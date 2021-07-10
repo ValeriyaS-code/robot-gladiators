fbd141766f0328940c86a4281268e6d5a910b451
ValeriyaS-code <85139016+ValeriyaS-code@users.noreply.github.com>
2021-07-10 09:05:46 -0400

    fight/skip bug fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,22 +4,35 @@ var randomNumber = function(min, max) {
   return value;
 };
 
+//fight or skip function
+var fightOrSkip = function() {
+var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+if (promptFight === "" || promptFight === null) {
+  window.alert("You need to provide a valid answer! Please try again.")
+  return fightOrSkip();
+}
+promptFight = promptFight.toLowerCasw();
+if (promptFight === "skip") {
+  var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+  if (confirmSkip) {
+    window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
+    playerInfo.playerMoney = Math.max(0, playerInfo.money - 10);
+    
+    return true; 
+  }
+ }
+ return false;
+};
+
+
 //fight function
 var fight = function(enemy) {
   while (playerInfo.health > 0 && enemy.health > 0) {
-
-    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
-    if (promptFight === "skip" || promptFight === "SKIP") {
-      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-      if (confirmSkip) {
-        window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
-        playerInfo.money = Math.max(0, playerInfo.money - 10);
-        console.log("playerInfo.money", playerInfo.money)
-        break;
-      }
+    if (fightOrSkip()) {
+      break;
     }
-
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
     enemy.health = Math.max(0, enemy.health - damage);
